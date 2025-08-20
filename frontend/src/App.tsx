0610a0e088088ee8609df0de3da44b3957eff07e
Abhijit Sahoo <shawabhijit370@gmail.com>
2025-08-20 22:05:34 +0530

    react vite app created for the frontend and intial packages are added

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-cyan-700 text-3xl font-bold">
+        Hello ClariPix
+      </div>
+    </>
+  )
+}
+
+export default App
